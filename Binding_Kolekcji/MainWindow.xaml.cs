0a898462ab08169943ed2b5727ea7c424612c619
Wiktor Gapiński <wiktor_gapinski@proton.me>
2025-01-09 00:05:15 +0100

    Add sorting by warehouse and product name

--- Binding_Kolekcji/MainWindow.xaml.cs
+++ Binding_Kolekcji/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.ComponentModel;
+using System.Windows.Data;
+
 
 namespace Binding_Kolekcji
 {
     public partial class MainWindow : Window
     {
-        // Deklarujemy ObservableCollection dla produktów
         private ObservableCollection<Produkt> ListaProduktow;
 
         public MainWindow()
@@ -16,17 +18,21 @@ namespace Binding_Kolekcji
 
         private void PrzygotujWiazanie()
         {
-            // Inicjalizacja ObservableCollection z danymi
             ListaProduktow = new ObservableCollection<Produkt>
-            {
-                new Produkt("01-11", "ołówek", 8, "Katowice 1"),
-                new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"),
-                new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"),
-                new Produkt("DZ-12", "długopis kulkowy", 280, "Katowice 2")
-            };
+    {
+        new Produkt("01-11", "ołówek", 8, "Katowice 1"),
+        new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"),
+        new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"),
+        new Produkt("DZ-12", "długopis kulkowy", 280, "Katowice 2")
+    };
 
-            // Ustawienie wiązania z ListView
             1stProdukty.ItemsSource = ListaProduktow;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(1stProdukty.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Magazyn", ListSortDirection.Ascending));
+
+            view.SortDescriptions.Add(new SortDescription("Nazwa", ListSortDirection.Ascending));
         }
+
     }
 }
