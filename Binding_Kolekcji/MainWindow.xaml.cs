59536428f8bb7d70638f417068931da2991d95a2
Wiktor Gapiński <wiktor_gapinski@proton.me>
2025-01-08 23:31:56 +0100

    Add project folder

---
+++ Binding_Kolekcji/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Binding_Kolekcji
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+
+        public Produkt(string symbol, string nazwa, int liczbaSztuk, string magazyn)
+        {
+            Symbol = symbol;
+            Nazwa = nazwa;
+            LiczbaSztuk = liczbaSztuk;
+            Magazyn = magazyn;
+        }
+    }
+
+}
